6dd740fd5c3b8c0e2fba387795bde00e97fb45a2
Phan Vĩnh Thái Hòa <thaihoaa246@gmail.com>
2026-04-15 21:23:40 +0700

    login function

---
+++ src/api/API_Auth.ts
@@ -0,0 +1,10 @@
+// import axios from "axios";
+// //import { LoginRequest, LoginResponse } from "@/interfaces/auth";
+// // API request login
+// export const API_SignIn = async (values: LoginRequest): Promise<LoginResponse> => {
+//   const res = await axios.post<LoginResponse>(
+//     "https://bookingphoto.onrender.com/auth/login",
+//     values
+//   );
+//   return res.data;
+// };
